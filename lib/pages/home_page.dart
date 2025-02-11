075343cf697544c7b86b31a3916905858eb13bd6
Igor <igor-skromnik@yandex.ru>
2025-02-12 00:09:50 +0300

    created glassmorphic textfield and seperated styles file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:ticker_app/res/styles/app_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -23,17 +26,17 @@ class HomePage extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Column(
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             'Good morning',
-                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                            style: AppStyles.aboveHeadlineText,
                           ),
-                          SizedBox(height: 10),
+                          const SizedBox(height: 10),
                           Text(
                             'Book Tickets',
-                            style: TextStyle(fontSize: 30),
+                            style: AppStyles.headlineText,
                           )
                         ],
                       ),
@@ -44,11 +47,50 @@ class HomePage extends StatelessWidget {
                         ),
                       )
                     ],
-                  )
+                  ),
+                  SizedBox(height: 30),
+                  GlassmorphicTextField(),
                 ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class GlassmorphicTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
+        child: Container(
+          padding: EdgeInsets.only(bottom: 5, top: 10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.white24),
+            color: Colors.transparent,
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                focusedErrorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                hintText: 'Senders name',
+                suffixIcon: Icon(HugeIcons.strokeRoundedSearch01),
+              ),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/res/styles/app_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF89CFF0);
+
+class AppStyles {
+  static Color primaryColor = primary;
+
+  static TextStyle headlineText =
+      const TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
+
+  static TextStyle aboveHeadlineText = const TextStyle(
+      color: Colors.grey, fontSize: 15, fontWeight: FontWeight.w500);
+}
